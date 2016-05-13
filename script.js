5653b6811d11e2fe83924a1134001d3444a6eb5e
Morgan Stone <morgan.stone@medseek.com>
2016-05-13 10:01:58 -0500

    MONA-138 - Initial design

--- script.js
+++ script.js
@@ -9,6 +9,61 @@
   function TestController() {
     var vm = this;
     vm.name = 'World!';
+
+    vm.buttons = {
+      next: {
+        title: 'Next'
+      }
+    };
+
+    vm.steps = [
+      {
+        name: 'Build Stuff',
+        id: 'step1',
+        active: false,
+        complete: true,
+        disabled: false,
+        steps: [
+          {
+            name: 'Step 1 sub A',
+            id: 'step1a',
+            active: false,
+            complete: true,
+            disabled: false
+          },
+          {
+            name: 'Step 1 sub B',
+            id: 'step1b',
+            active: false,
+            complete: true,
+            disabled: false
+          }
+        ]
+      },
+      {
+        name: 'Do Stuff',
+        id: 'step2',
+        active: true,
+        complete: false,
+        disabled: false
+      },
+      {
+        name: 'Do Stuff even better',
+        id: 'step3',
+        active: false,
+        complete: false,
+        disabled: true
+      }
+    ];
+
+    vm.callbacks = {
+      nextClick: nextClick
+    };
+
+    function nextClick(wizardId, wizardState) {
+      console.log('boomddddd');
+    }
+
   }
 
 })();
