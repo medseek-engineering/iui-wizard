609c3bb67566725e37432c242cbbbae6e240a573
Morgan Stone <morgan.stone@medseek.com>
2016-05-16 17:07:48 -0500

    MONA-138 - step flow test

--- script.js
+++ script.js
@@ -2,37 +2,68 @@
   'use strict';
 
   angular
-    .module('app', ['ui.bootstrap','iui.wizard'])
+    .module('app', ['ui.bootstrap', 'iui.wizard', 'ui.router'])
+    .config(appStates)
     .constant('_', window._)
     .controller('TestController', TestController);
 
+  function appStates($stateProvider) {
+    $stateProvider
+      .state('wizardStep1', {
+        url: '',
+        views: {
+          'primaryView': {
+            templateUrl: '/step1.html',
+            controller: 'TestController as test'
+          }
+        }
+      })
+      .state('physicianDetail', {
+        url: '/physicians/:physicianId',
+        views: {
+          'primaryView': {
+            templateUrl: '/physician-detail.html',
+            controller: 'ManagePhysiciansController as managePhysicians'
+          }
+        }
+      })
+  }
+
   function TestController() {
     var vm = this;
-    vm.name = 'World!';
-
-    vm.buttons = {
-      next: {
-        title: 'Next'
-      }
-    };
 
     vm.steps = [
       {
-        name: 'Build Stuff',
+        name: 'Define List Details',
         id: 'step1',
         active: false,
         complete: true,
         disabled: false,
+        steps: []
+      },
+      {
+        name: 'Define Locations',
+        id: 'step2',
+        active: false,
+        complete: true,
+        disabled: false
+      },
+      {
+        name: 'Manage Lists',
+        id: 'step3',
+        active: true,
+        complete: false,
+        disabled: false,
         steps: [
           {
-            name: 'Step 1 sub A',
+            name: 'Past Lists',
             id: 'step1a',
-            active: false,
+            active: true,
             complete: true,
             disabled: false
           },
           {
-            name: 'Step 1 sub B',
+            name: 'Seed Lists',
             id: 'step1b',
             active: false,
             complete: true,
@@ -41,15 +72,29 @@
         ]
       },
       {
-        name: 'Do Stuff',
-        id: 'step2',
-        active: true,
+        name: 'Refine Criteria',
+        id: 'step4',
+        active: false,
         complete: false,
-        disabled: false
+        disabled: true
       },
       {
-        name: 'Do Stuff even better',
-        id: 'step3',
+        name: 'Manage Segments',
+        id: 'step5',
+        active: false,
+        complete: false,
+        disabled: true
+      },
+      {
+        name: 'Targeting',
+        id: 'step6',
+        active: false,
+        complete: false,
+        disabled: true
+      },
+      {
+        name: 'Summary',
+        id: 'step7',
         active: false,
         complete: false,
         disabled: true
@@ -57,11 +102,16 @@
     ];
 
     vm.callbacks = {
-      nextClick: nextClick
+      nextClick: nextClick,
+      goToStep: goToStep
     };
 
     function nextClick(wizardId, wizardState) {
       console.log('boomddddd');
+    }
+
+    function goToStep(wizardStep) {
+      console.log(wizardStep);
     }
 
   }
